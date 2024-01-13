7137fb8cb9739c11a61b3e1ddcdf099228390658
Pavel-Fomchenkov <donotwritemail@gmail.com>
2024-01-13 15:21:08 +0300

    Selection sort

--- src/main/java/pro/sky/javacoursepart2/Main.java
+++ src/main/java/pro/sky/javacoursepart2/Main.java
@@ -1,13 +1,14 @@
 package pro.sky.javacoursepart2;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(List.of(15, 0, -17, 2, 100, -178, -555, 37, 42, 99));
-        list.sort(Comparator.naturalOrder());
+//        list.sort(Comparator.naturalOrder());
+        selectionSort(list);
+
         System.out.println(list); // [-555, -178, -17, 0, 2, 15, 37, 42, 99, 100]
         System.out.println(binary_search(list, 0));
     }
@@ -24,6 +25,19 @@ public class Main {
             } else low = mid + 1;
         }
         return -1;
+    }
+
+    public static void selectionSort(List<Integer> list) {
+        int temp;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(i)) {
+                    temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
     }
 
 
